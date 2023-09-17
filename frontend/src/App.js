246caf23d4bcd2260b969f823a5d5d020b2a9e07
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-09-17 01:39:14 -0400

    buttons

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,13 @@ import MapContainer from "./MapContainer";
 function App() {
   return (
     <div className="App">
+      <div className='black-container'>
+        <p>Click on a garden to see more details</p>
+        <div className='join-button-container'>
+          <button className='join-button'>Join Garden</button>
+        </div>
+      </div>
+      
       <main>
         <MapContainer />
       </main>
