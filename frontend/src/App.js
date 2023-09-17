06ea02209353624b6f8b2ff5b6bd65b5c8884132
Anshul Shirude <34725764+AnshulShirude@users.noreply.github.com>
2023-09-17 02:37:07 -0400

    images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import MapContainer from "./MapContainer";
+import plot from "./plot_no_background.png";
 
 function App() {
   return (
     <div className="App">
       <div className='black-container'>
+        <div className='plot-name'>Plot</div>
+        <img src={plot} alt="Plot Image" className='plot-image'/>
         <p>Click on a garden to see more details</p>
         <div className='join-button-container'>
           <button className='join-button'>Join Garden</button>
